2d78ba8aab8d0ce47aa445270584995106747039
Nellurijahnavi <146921091+Nellurijahnavi@users.noreply.github.com>
2025-03-14 20:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-app = Flask(__name__)
+app = Flask(app = Flask(__name__, template_folder='templates'))
 @app.route('/', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
