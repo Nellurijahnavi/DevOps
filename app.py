9f68208eacdc53f95e2f0fadfe6185d6d7844f09
Nellurijahnavi <146921091+Nellurijahnavi@users.noreply.github.com>
2025-03-15 12:18:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, render_template
-app = Flask(__name__, template_folder='templates')
-@app.route('/', methods=['GET', 'POST'])
+app = Flask(__name__)
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
+     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
-    
-        return render_template('succes.html')
-    return render_template('reg.html')
+        return render_template('sucess.html',name=name,email=email)
+     return render_template('reg.html')
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000)
